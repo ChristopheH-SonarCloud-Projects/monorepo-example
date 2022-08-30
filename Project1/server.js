5ac1719fdc1a6c7249862068b895ef6539b838e4
Christophe Havard <havard.christophe@gmail.com>
2022-08-30 12:32:07 +0200

    added non compliant method

--- Project1/server.js
+++ Project1/server.js
@@ -12,7 +12,17 @@ const start = () => {
   app.get('*', (req, res) => {
     res.sendFile(`index.html`, { root: www });
   });
+
+  nonCompliantCode();
+
   app.listen(port, () => captains.log(`listening on http://localhost:${port}`));
 };
+
+// https://rules.sonarsource.com/javascript/RSPEC-5131
+function nonCompliantCode(req, res){
+  const tainted = req.query.name;
+  res.send(tainted); // Noncompliant
+}
+
 
 start();
